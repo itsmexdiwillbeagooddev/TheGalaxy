9e9ebece91ab769fe0cb7da657619daf75e3e93c
MinatoUnder <44467517+MinatoUnder@users.noreply.github.com>
2018-11-25 22:45:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -227,6 +227,8 @@ message.channel.send(IzRo);
 });
 }
 
+
+client.on("message", (message) => {
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
